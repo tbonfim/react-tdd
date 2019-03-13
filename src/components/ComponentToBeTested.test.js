71e6b9fd281c977d4cbcb3929dede3d0d0012732
Thiago Bonfim <thiakau@gmail.com>
2019-03-13 13:33:12 -0700

    Step 3 - Create failing test to 2 input fields inside the component

--- src/components/ComponentToBeTested.test.js
+++ src/components/ComponentToBeTested.test.js
@@ -2,8 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ComponentToBeTested from './ComponentToBeTested';
 
+import Enzyme, {mount} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({adapter: new Adapter()});
+
 it('renders ComponentToBeTested without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<ComponentToBeTested />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+it('Should have at least 2 input fields', () => {
+  const wrapper = mount(<ComponentToBeTested />);
+  expect(wrapper.find('input')).toHaveLength(2);
 });
